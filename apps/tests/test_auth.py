64dec2266f2e4d5fd02ee788d8edce1f47b8e132
vinmwega <vinmwega@gmail.com>
2021-11-26 21:03:22 +0300

    Added test skeleton codes for later implementation

---
+++ apps/tests/test_auth.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestAuth(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
